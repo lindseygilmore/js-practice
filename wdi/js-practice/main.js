e80556f32c3aa3a23eedd153e26629eb32f95e52
lindseygilmore <lindseygilmore21@gmail.com>
2017-09-06 17:53:28 -0500

    myVar practice

--- wdi/js-practice/main.js
+++ wdi/js-practice/main.js
@@ -1,11 +1,14 @@
 function b(){
-	console.log('Called b!');
+	var myVar;
+	console.log(myVar);
 }
 
-b();
+function a(){
+	var myVar = 2;
+	console.log(myVar);
+	b();
+}
 
-console.log(a);
-
-var a = 'hello world';
-
-console.log(a);
+var myVar = 1;
+console.log(myVar);
+a();
